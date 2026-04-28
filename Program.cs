29f361d8eb100d6a29f29ec0d345c6e219563804
Benjamin Terzic <52902523+CloneMalone@users.noreply.github.com>
2026-04-27 19:11:26 -0500

    Integrate EF Core and MVC into the application
    Added Entity Framework Core setup for SQL Server and MVC support.

--- Program.cs
+++ Program.cs
@@ -1,29 +1,32 @@
+using DnDManager.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add MVC
 builder.Services.AddControllersWithViews();
+
+// Add EF Core with SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DnDManager")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Campaign}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Campaign}/{action=Index}/{id?}");
 
 app.Run();
