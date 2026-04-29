8b2e5b3d5fc7f7aef4c4f56dc26e233f3fa35107
Nathanael-Tekalign <156130272+Nathanael-Tekalign@users.noreply.github.com>
2026-04-28 21:12:57 -0500

    Update Character.cs

--- Models/Character.cs
+++ Models/Character.cs
@@ -5,5 +5,39 @@ namespace DnDManager.Models
     public class Character
     {
         // Properties go here
+        [Key]
+public int CharacterId { get; set; }   // Primary Key
+
+[Required]
+public int CampaignId { get; set; }    // Foreign Key
+
+[Required]
+[MaxLength(100)]
+public string Name { get; set; } = string.Empty;
+
+[Required]
+[MaxLength(50)]
+public string Class { get; set; } = string.Empty;
+
+[Required]
+[MaxLength(50)]
+public string Race { get; set; } = string.Empty;
+
+[Range(1, 20)]
+public int Level { get; set; }
+
+[Range(0, int.MaxValue)]
+public int CurrentHp { get; set; }
+
+[Range(1, int.MaxValue)]
+public int MaxHp { get; set; }
+
+[Range(0, 50)]
+public int ArmorClass { get; set; }
+
+public int InitiativeBonus { get; set; }
+
+[MaxLength(100)]
+public string? Status { get; set; }
     }
 }
